e4ac8afe8f8a0bdf9924841692d78225826b75f6
MahipatBriotechno <102782100+MahipatBriotechno@users.noreply.github.com>
2024-05-06 23:25:12 +0530

    add whishlist work

---
+++ src/app/components/WishlistHeartButton/index.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React, { useState } from "react";
+
+const WishlistHeartButton = () => {
+  const [isInWishList, setIsInWishList] = useState(false);
+
+  const toggleWishlist = async () => {
+    // Implement the logic to add or remove the item from the wishlist
+    // This could involve calling an API endpoint or updating local state
+    setIsInWishList(!isInWishList);
+  };
+
+  return (
+    <button
+      type="button"
+      onClick={toggleWishlist}
+      className={`ml-4 py-3 px-3 rounded-md flex items-center justify-center ${
+        isInWishList ? "text-red-600" : "text-gray-400"
+      } hover:bg-gray-100`}
+    >
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill={`${isInWishList ? "#EB4C6B" : "white"}`}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M12.62 20.8096C12.28 20.9296 11.72 20.9296 11.38 20.8096C8.48 19.8196 2 15.6896 2 8.68961C2 5.59961 4.49 3.09961 7.56 3.09961C9.38 3.09961 10.99 3.97961 12 5.33961C13.01 3.97961 14.63 3.09961 16.44 3.09961C19.51 3.09961 22 5.59961 22 8.68961C22 15.6896 15.52 19.8196 12.62 20.8096Z"
+          stroke={`${isInWishList?'#EB4C6B':'#292D32'}`}
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
+
+      <span className="sr-only">
+        {isInWishList ? "Remove from wishlist" : "Add to wishlist"}
+      </span>
+    </button>
+  );
+};
+
+export default WishlistHeartButton;
