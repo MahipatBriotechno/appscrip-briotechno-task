9b22a03e2f224195c00a108c8edd1c4afd602370
MahipatBriotechno <102782100+MahipatBriotechno@users.noreply.github.com>
2024-05-07 19:12:47 +0530

    fix issue

--- src/app/components/productCard/index.tsx
+++ src/app/components/productCard/index.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import heart from "../../assets/svg/heart.svg";
 import heartRed from "../../assets/svg/heart-red.svg";
 import WishlistHeartButton from "../WishlistHeartButton";
+import Link from "next/link";
 
 interface ProductCardProps {
   image: any; // Path to the image
@@ -53,11 +54,11 @@ const ProductCard: React.FC<ProductCardProps> = ({
       </div>
       <div className="flex items-start lg:items-end justify-between gap-2">
         <div>
-          <h4 className="text-sm lg:text-lg font-bold leading-5 line-clamp-1 mb-2">
+          <h4 className="text-sm lg:text-lg font-bold leading-5 line-clamp-1 mb-2 uppercase">
             {title}
           </h4>
           <h6 className="text-gray-600 text-[10px] lg:text-sm font-normal">
-            Sign in or Create an account to see pricing
+            <Link href="#">Sign in</Link> or Create an account to see pricing
           </h6>
           {/* {like ? <Image src={heartRed} alt="" /> : <Image src={heart} alt="" />} */}
         </div>
